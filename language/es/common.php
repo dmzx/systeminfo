2af6e47fc6cec36e04d36c26af050bd21f18ae4a
dmzx <info@dmzx-web.net>
2015-09-20 18:21:38 +0200

    Spanish translation

---
+++ language/es/common.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* @package System Info
+* @copyright (c) 2015 dmzx - http://dmzx-web.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'SYSTEM_INFO'				=> 'Información del sistema',
+	'SYSTEM_UPTIME'			=> 'Servidor en linea:',
+	'SYSTEM_LOAD'				=> 'Carga media:',
+	'SYSTEM_NAME'				=> 'Sistema:',
+	'SYSTEM_DAYS'				=> 'dias,',
+	'SYSTEM_HOURS'			 => 'horas,',
+	'SYSTEM_MIN'				=> 'minutos y',
+	'SYSTEM_SECS'				=> 'segundos',
+	'BOARD_VERSION'			=> 'Versión del foro',
+));
